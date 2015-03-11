d72bebc96cb3212af08ccdf06936e8013b17b34c
Michael Fairchild <mfairchild365@gmail.com>
2015-03-11 13:52:30 -0500

    Add a testing script to clean up liferay content

---
+++ fix_liferay.php
@@ -0,0 +1,46 @@
+<?php
+ini_set('display_errors', true);
+
+require_once __DIR__ . '/unl_migration.php';
+
+$baseUrl          = '';
+$frontierPath     = null;
+$frontierUser     = null;
+$frontierPass     = null;
+$ignoreDuplicates = null;
+$useLiferayCode   = true;
+$useLiferayTitles = false;
+
+$migration_tool = new UNL_Migration_Tool($baseUrl, $frontierPath, $frontierUser, $frontierPass, $ignoreDuplicates, $useLiferayCode, $useLiferayTitles);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Liferay main content fixer</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <h1>Fix Liferay main content</h1>
+  <p>This will strip out/replace old lefray DIVs and Content</p>
+  <form action="fix_liferay.php" method="post">
+    <label for="content_to_fix">The HTML content to fix</label>
+    <br />
+    <textarea id="content_to_fix" name="content" cols="100" rows="15"></textarea>
+    <br />
+    <input type="submit" />
+  </form>
+  
+  <?php 
+  if (isset($_POST['content'])):
+    $fixed_content = $migration_tool->_perform_liferay_maincontent_replacements($_POST['content']);
+    ?>
+    <br />
+    <label for="fixed_html">Fixed HTML</label>
+    <br />
+    <textarea id="fixed_html" name="fixed_html" cols="100" rows="15" readonly="readonly"><?php echo $fixed_content ?></textarea>
+  <?php endif; ?>
+<!-- page content -->
+</body>
+</html>
